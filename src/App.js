fa98430adee2817533e17ff9b1125389cbd99a33
Aaron Boyd <a.boyd91@yahoo.com>
2022-07-29 13:05:01 -0700

    add buttons for adding todo, clearing completed todo and div to show how many todos are left

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <>
       <TodoList />
       <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>O left to do</div>
     </>
   )
 }
